2ffadbf3e6747d68e725fe4bd6cd89dde77338a7
Dan Beasley <dannyleebeasley@gmail.com>
2021-08-19 17:53:43 -0400

    added basic CRUD routes in controllers

--- app/controllers/pedals_controller.rb
+++ app/controllers/pedals_controller.rb
@@ -1,2 +1,41 @@
 class PedalsController < ApplicationController
+    rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+
+    wrap_parameters format: []
+
+    def index
+        pedals = Pedal.all 
+        render json: pedals, status: :ok 
+    end
+
+    def create 
+        pedal = pedal.create(pedal_params)
+        render json: pedal, status: :created
+    end
+
+    def update
+        pedal = find_pedal
+        pedal.update(pedal_params)
+        render json: pedal, status: :accepted 
+    end
+
+    def destroy
+        pedal = find_pedal
+        pedal.destroy
+        head :no_content
+    end
+
+    private
+
+    def pedal_params
+        params.permit(:name, :effect_type, :price, :stereo, :number_of_inputs, :number_of_outputs, :image, :user_id, :pedal_board_id, :category)
+    end
+
+    def find_pedal
+        Pedal.find(params[:id])
+    end
+
+    def render_not_found_response
+        render json: { error: pedal.errors.full_messages }, status: :not_found 
+    end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,42 @@
 class UsersController < ApplicationController
+    rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+
+    wrap_parameters format: []
+
+    def index
+        users = User.all 
+        render json: users, status: :ok 
+    end
+
+    def create 
+        user = User.create(user_params)
+        render json: user, status: :created
+    end
+
+    def update
+        user = find_user
+        user.update(user_params)
+        render json: user, status: :accepted 
+    end
+
+    def destroy
+        user = find_user
+        user.destroy
+        head :no_content
+    end
+
+    private
+
+    def user_params
+        params.permit(:name, :username, :email, :password_digest)
+    end
+
+    def find_user
+        User.find(params[:id])
+    end
+
+    def render_not_found_response
+        render json: { error: user.errors.full_messages }, status: :not_found 
+    end
+    
 end
